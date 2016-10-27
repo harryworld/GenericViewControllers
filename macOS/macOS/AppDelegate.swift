262df9e8c9b994f9c9beba9bf192de83db354fa0
Harry Ng <harryworld@gmail.com>
2016-10-27 19:05:09 +0800

    Create new projects

---
+++ macOS/macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS
+//
+//  Created by Harry Ng on 27/10/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
